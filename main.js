531726d377c9e45898b83b734e90ff025e30179f
Eudemon <eudemonrandy@hotmail.com>
2024-04-13 14:46:45 -0400

    pull directly from  json source

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const fs = require('fs');
 const download = require('image-downloader');
+const https = require('https');
 
-const rawData = fs.readFileSync('cardinfo.php.json');
-const data = JSON.parse(rawData).data;
+async function getJson() {
+  const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, resp => {
+        let data = '';
+
+        resp.on('data', chunk => {
+          data += chunk;
+        });
+
+        resp.on('end', () => {
+          const info = JSON.parse(data).data;
+          resolve(info);
+        });
+      })
+      .on('error', err => {
+        console.log(`Error: ${err.message}`);
+        reject(err);
+      });
+  });
+}
 
 function Download(card) {
   if (typeof card.card_images[0].image_url != 'undefined') {
@@ -26,11 +46,16 @@ function Download(card) {
 }
 
 // start here
-let index = 12700;
+let index = 13000;
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function start() {
+  let data = ''
+  await getJson().then(
+    info => { data = info }
+  ).catch(err => { process.exit() })
+
   for (let key = index; key < data.length; key++) {
     const card = data[key];
 
